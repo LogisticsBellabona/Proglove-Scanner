c1e5ba8abe3c1df0108bbbcaf10945361b459a24
LogisticsBellabona <roberto@bellabona.com>
2025-10-24 16:19:36 +0200

    Update app-logic.js

--- app-logic.js
+++ app-logic.js
@@ -38,15 +38,14 @@ const USERS = [
 
 // ✅ Correct Firebase Config for proglove-bowl-tracker
 const firebaseConfig = {
-  apiKey: "AIzaSyDya1dDRSeQmuKnpraSoSoTjauLlJ_J94I",
+  apiKey: "...",
   authDomain: "proglove-bowl-tracker.firebaseapp.com",
-  databaseURL: "https://proglove-bowl-tracker-default-rtdb.firebaseio.com",
+  databaseURL: "https://proglove-bowl-tracker-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "proglove-bowl-tracker",
-  storageBucket: "proglove-bowl-tracker.appspot.com",
+  storageBucket: "proglove-bowl-tracker.appspot.com",  // adjust bucket if needed
   messagingSenderId: "280001054969",
   appId: "1:280001054969:web:a0792a228ea2f1c5c9ba28"
 };
-
 
 // ------------------- UTILITIES -------------------
 function showMessage(message, type) {
