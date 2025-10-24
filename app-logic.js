8c15d987db46ee90f1096146d9bd06a073ce7562
LogisticsBellabona <roberto@bellabona.com>
2025-10-24 18:22:39 +0200

    Update app-logic.js

--- app-logic.js
+++ app-logic.js
@@ -1,4 +1,4 @@
-/* app-logic.js
+/* ap-logic.js
    Complete single-file logic for ProGlove Bowl Tracking System
    - Works with Firebase Realtime DB (project: proglove-scanner)
    - Clean scan handling (kitchen + return)
